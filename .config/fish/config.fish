8498a8a0840a9a8ba86dfbfaada26ae45a400a6f
tinfoil-hat <mail@tinfoil-hat.net>
2026-08-30 23:16:32 +0200

    Initial Commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,119 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# ===================== COLORS ==================== #
+set -g fish_color_command --bold green
+set -g fish_color_error red
+set -g fish_color_quote yellow
+set -g fish_color_param white
+# ===================== COLORS ==================== #
+
+
+# ==================== ALIASES ==================== #
+alias ausstime "TZ=Australia/Sydney date"
+alias backup-local "~/Software/git/rsync-time-backup/rsync_tmbackup.sh ~/ /media/anon/8TB-int/Backup/LMDE7/ ~/Software/git/rsync-time-backup/exclude.txt"
+alias backup-remote "rclone copy /media/anon/8TB-int/Backup/LMDE7/latest/ Opendrive-Crypt:/ --skip-links -L --progress --transfers 8"
+alias berliner-rundfunk "mpv http://stream.berliner-rundfunk.de/brf/mp3-128/internetradio"
+alias cast-ra "sudo openvpn --config ~/Sync/Essentials/ra.ovpn"
+alias cast-vps "sudo openvpn --config ~/Sync/Essentials/vps.ovpn"
+alias config "git --git-dir=/home/anon/.cfg/ --work-tree=/home/anon"
+alias copy-website "/home/anon/Software/scripts/website-build-n-copy.sh"
+alias disk "duf"
+alias drop "~/Software/bin/plik"
+alias errors "sudo lnav -r /var/log"
+alias fff "fastfetch"
+alias ff "fastfetch -c .config/fastfetch/mac.jsonc"
+alias gw "~/Software/scripts/gw.sh"
+alias hardware "inxi -Fnxxz"
+alias installation-age "stat -c %w / | cut -d\  -f1"
+alias pc-age "sudo dmidecode | egrep '199[0-9]|20[0-3][0-9]'"
+alias kink "sudo tomb open ~/Videos/kink.tomb -k ~/Dokumente/tomb.key"
+alias ll "ls -lah --group-directories-first"
+alias reechat "ssh chrissly@vps -t screen -rd weechat"
+alias reverse-playback "mpv --play-direction=backward"
+alias rss "newsboat"
+alias screencap "~/Software/scripte/ffmpeg.sh"
+alias timer "~/Software/python/termdown/bin/termdown"
+alias torrent "aria2c --follow-torrent=mem --seed-time=0 --max-upload-limit=0 -j 10"
+alias ustime "TZ=America/New_York date"
+alias vanilla "sudo tomb open ~/Videos/vanilla.tomb -k ~/Dokumente/tomb.key"
+alias webserver "python3 -m http.server 8001"
+alias check-port "nc -zv"
+alias screenfetch "cat ~/Dokumente/anonatreality.txt"
+# ==================== ALIASES ==================== #
+
+# ==================== EXPORTS ==================== #
+export EDITOR="vim"
+export BROWSER="librewolf"
+# less with syntax highlighting
+export LESSOPEN="| pygmentize -g %s"
+export LESS="-R"
+# ==================== EXPORTS ==================== #
+
+# =================== FUNCTIONS =================== #
+# Forget commands matching a search term
+function history_forget-yt-dlp
+    history delete --contains "yt-dlp"
+    history save
+end
+# Git autocommit
+function dotfiles_autoupdate
+    config add -u
+    and config commit -m "Update "(date +"%Y-%m-%d %H:%M")" "(uname -s)"/"(uname -m)
+    and config push
+end
+
+# Tempoary File Upload
+function up
+    if test -z "$argv[1]"
+        echo "Usage: up <file>"
+        echo "Example: up image.jpg"
+        return 1
+    end
+
+    if test ! -f "$argv[1]"
+        echo "Error: File '$argv[1]' not found!"
+        return 1
+    end
+
+    # -s = silent (no progress bar), -w = print the final URL on its own line
+    curl -s -w '\n' -F "file=@$argv[1]" https://x0.at/
+end
+
+# Tmux Startup
+function tt
+    tmux new-session -ds 'start' 'htop'
+
+    # Split horizontally: right half for newsboat
+    tmux split-window -h 'newsboat'
+
+    # Go back to the left pane (pane 0) and split it vertically
+    tmux select-pane -t 1
+    tmux split-window -v "tty-clock -c -f '%Y-%m-%d %H:%M:%S'"
+
+    tmux -2 attach-session -d
+end
+
+function ttt
+    tmux new-session -ds 'start' 'htop'
+
+    # Split horizontally: right half for newsboat
+    tmux split-window -h 'newsboat'
+
+    # Go back to the left pane (pane 0) and split it vertically
+    tmux select-pane -t 1
+    tmux split-window -v "tty-clock -c -f '%Y-%m-%d %H:%M:%S'"
+
+    tmux new-window 'reechat'
+    tmux select-window -t 1
+    tmux -2 attach-session -d
+end
+
+# =================== FUNCTIONS =================== #
+
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/anon/.lmstudio/bin
+# End of LM Studio CLI section
+
---
+++ .vimrc
@@ -0,0 +1,136 @@
+syntax on
+colorscheme lunaperche
+
+set nocompatible
+set number
+set cursorline
+set tabstop=2
+set ignorecase
+set hlsearch
+set completeopt+=menuone
+set completeopt+=noselect
+set shortmess+=c
+set belloff+=ctrlg
+
+set statusline=
+set statusline+=%{Changestatuslinecolor()}
+set statusline+=%1*\ %{g:currentmode[mode()]}
+set statusline+=%3*\ %f\ %4*\ 
+set statusline+=%=\ 
+set statusline+=%3*\ %l\ of\ %L\ %2*\ 
+set statusline+=%2*%{&filetype}\ 
+set noshowmode
+
+set laststatus=2
+set statusline=
+set statusline+=\ %f%m%r%h%w\              " file + flags
+set statusline+=%=                         " right align
+set statusline+=\ %y                       " filetype
+set statusline+=\ %{&fileencoding}
+set statusline+=\ [%{&fileformat}]
+set statusline+=\ %l/%L\ (%p%%)\ %c\       " line/total (%) column
+
+"tabulation"
+set copyindent
+set preserveindent
+set softtabstop=0
+set shiftwidth=4
+set tabstop=4
+set noexpandtab
+set noshiftround
+
+"searching"
+set incsearch
+set hlsearch
+set ignorecase
+set smartcase
+
+"colorscheme without colorscheme plugin"
+hi linenr ctermfg=0
+hi cursorline ctermbg=NONE ctermfg=NONE cterm=NONE
+hi cursorlinenr ctermbg=NONE ctermfg=NONE cterm=NONE
+hi comment ctermfg=8
+hi pmenu ctermbg=0 ctermfg=NONE
+hi pmenusel ctermbg=4 ctermfg=0
+hi pmenusbar ctermbg=0
+hi pmenuthumb ctermbg=7
+hi matchparen ctermbg=0 ctermfg=NONE
+hi search ctermbg=0 ctermfg=NONE
+hi vertsplit ctermbg=0 ctermfg=0 cterm=NONE
+hi tablinefill ctermbg=NONE ctermfg=NONE cterm=NONE
+hi tabline ctermbg=NONE ctermfg=0
+hi tablinesel ctermbg=6 ctermfg=7
+hi group1 ctermbg=NONE ctermfg=0
+hi group2 ctermbg=NONE ctermfg=0
+match group1 /\s\+$/
+match group2 /\t/
+
+"tab autocomplete without autocomplete plugin"
+inoremap <expr> <Tab> TabComplete()
+fun! TabComplete()
+if getline('.')[col('.') - 2] =~ '\K' || pumvisible()
+return "\<C-P>"
+else
+return "\<Tab>"
+endif
+endfun
+set completeopt=menu,menuone,noinsert
+inoremap <expr> <CR> pumvisible() ? "\<C-Y>" : "\<CR>"
+autocmd InsertCharPre * call AutoComplete()
+fun! AutoComplete()
+if v:char =~ '\K'
+\ && getline('.')[col('.') - 4] !~ '\K'
+\ && getline('.')[col('.') - 3] =~ '\K'
+\ && getline('.')[col('.') - 2] =~ '\K'
+\ && getline('.')[col('.') - 1] !~ '\K'
+call feedkeys("\<C-P>", 'n')
+end
+endfun
+
+
+"statusline without statusline plugin"
+let g:currentmode={
+\ 'n'  : 'Normal ',
+\ 'no' : 'N·Operator Pending ',
+\ 'v'  : 'Visual ',
+\ 'V'  : 'V·Line ',
+\ 'x22' : 'V·Block ',
+\ 's'  : 'Select ',
+\ 'S'  : 'S·Line ',
+\ 'x19' : 'S·Block ',
+\ 'i'  : 'Insert ',
+\ 'R'  : 'Replace ',
+\ 'Rv' : 'V·Replace ',
+\ 'c'  : 'Command ',
+\ 'cv' : 'Vim Ex ',
+\ 'ce' : 'Ex ',
+\ 'r'  : 'Prompt ',
+\ 'rm' : 'More ',
+\ 'r?' : 'Confirm ',
+\ '!'  : 'Shell ',
+\ 't'  : 'Terminal '
+\}
+
+hi user1 ctermbg=1 ctermfg=0
+hi user2 ctermbg=4 ctermfg=0
+hi user3 ctermbg=0 ctermfg=NONE
+hi user4 ctermbg=NONE ctermfg=NONE
+hi statusline ctermbg=0 ctermfg=NONE
+hi statuslinenc ctermbg=0 ctermfg=0
+
+function! Changestatuslinecolor()
+if (mode() =~# '\v(n|no)')
+exe 'hi! user1 ctermbg=1 ctermfg=0'
+elseif (mode() =~# '\v(v|V)' || g:currentmode[mode()] ==# 'Visual Block' || get(g:currentmode, mode(), '') ==# 't')
+exe 'hi! user1 ctermbg=5 ctermfg=0'
+elseif (mode() ==# 'i')
+exe 'hi! user1 ctermbg=2 ctermfg=0'
+elseif (mode() ==# 'R')
+exe 'hi! user1 ctermbg=3 ctermfg=0'
+else
+exe 'hi! user1 ctermbg=1 ctermfg=0'
+endif
+return ''
+endfunction
+
+
